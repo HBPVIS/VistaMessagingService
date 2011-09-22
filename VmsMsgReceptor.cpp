c5aaeec36dae14a25325a031e97e239c2ef305e5
flat_ma <flat_ma@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2011-09-22 12:24:40 +0000

    ADD: VmsMsgHandler VmsMsgReceptor
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/VistaAddonLibs/VistaMessagingService/trunk@23476 33c38b33-09d2-4561-8d4d-1bfa2f58f885

---
+++ VmsMsgReceptor.cpp
@@ -0,0 +1,94 @@
+/*============================================================================*/
+/*       1         2         3         4         5         6         7        */
+/*3456789012345678901234567890123456789012345678901234567890123456789012345678*/
+/*============================================================================*/
+/*                                             .                              */
+/*                                               RRRR WW  WW   WTTTTTTHH  HH  */
+/*                                               RR RR WW WWW  W  TT  HH  HH  */
+/*      Header   :	VmsMsgFactory.cpp RRRR   WWWWWWWW  TT  HHHHHH  */
+/*                                               RR RR   WWW WWW  TT  HH  HH  */
+/*      Module   :  			                 RR  R    WW  WW  TT  HH  HH  */
+/*                                                                            */
+/*      Project  :  			                   Rheinisch-Westfaelische    */
+/*                                               Technische Hochschule Aachen */
+/*      Purpose  :                                                            */
+/*                                                                            */
+/*                                                 Copyright (c)  1998-2001   */
+/*                                                 by  RWTH-Aachen, Germany   */
+/*                                                 All rights reserved.       */
+/*                                             .                              */
+/*============================================================================*/
+/*                                                                            */
+/*    THIS SOFTWARE IS PROVIDED 'AS IS'. ANY WARRANTIES ARE DISCLAIMED. IN    */
+/*    NO CASE SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DAMAGES.    */
+/*    REDISTRIBUTION AND USE OF THE NON MODIFIED TOOLKIT IS PERMITTED. OWN    */
+/*    DEVELOPMENTS BASED ON THIS TOOLKIT MUST BE CLEARLY DECLARED AS SUCH.    */
+/*                                                                            */
+/*============================================================================*/
+// $Id: $
+
+/*============================================================================*/
+/* INCLUDES																	  */
+/*============================================================================*/
+
+#include "VmsMsgReceptor.h"
+#include "VmsMsgReceiver.h"
+#include "VmsMsgHandler.h"
+#include "VmsEndpointFactory.h"
+#include "VmsEndpointFactory.h"
+#include <sstream>
+
+using namespace std;
+
+VmsMsgReceptor::VmsMsgReceptor(zmq::context_t *pContext, const std::string strIp, const unsigned int iPort)
+{
+	VmsEndpointFactory oFactory = VmsEndpointFactory(pContext);
+	std::stringstream ss;
+	ss << "tcp://" << strIp << ":" << iPort;
+	m_pMsgReceiver = oFactory.CreateReceiver(ss.str());
+}
+
+VmsMsgReceptor::~VmsMsgReceptor(void)
+{
+	delete m_pMsgReceiver;
+	for(vector<VmsMsgHandler*>::iterator it=m_oHandlers.begin();it<m_oHandlers.end();it++ )
+	{
+		delete *it;
+	}
+}
+
+int VmsMsgReceptor::ProcessIncomingMsg()
+{
+
+	VmsMsg * pMsg=m_pMsgReceiver->ReceiveMsg();
+	unsigned int id=pMsg->GetType();
+	
+	if(id>m_oHandlers.size())
+	{
+		printf("[VmsMsgReceptor::ProcessIncomingMsg] MsgType not registered: 1");
+		return -1;
+	}
+	VmsMsgHandler * h = m_oHandlers[id];
+	if(!h)
+	{
+		printf("[VmsMsgReceptor::ProcessIncomingMsg] MsgType not registered: 2");
+		return -1;
+	}
+	h->HandleMessage(pMsg);
+	return 0;
+}
+	
+int VmsMsgReceptor::RegisterHandler(VmsMsgHandler *pHandler, int id)
+{
+	int len = m_oHandlers.size();
+	if(id>=len)
+	{
+		m_oHandlers.resize(id+1);
+		for(int i=len;i<id;i++)
+		{
+			m_oHandlers[i]=NULL;
+		}
+	}
+	m_oHandlers[id]=pHandler;
+	return 0;
+}
---
+++ VmsMsgReceptor.h
@@ -0,0 +1,78 @@
+/*                                               RRRR WW  WW   WTTTTTTHH  HH  */
+/*                                               RR RR WW WWW  W  TT  HH  HH  */
+/*      Header   :	VmsStringMsg.h				 RRRR   WWWWWWWW  TT  HHHHHH  */
+/*                                               RR RR   WWW WWW  TT  HH  HH  */
+/*      Module   :  			                 RR  R    WW  WW  TT  HH  HH  */
+/*                                                                            */
+/*      Project  :  			                   Rheinisch-Westfaelische    */
+/*                                               Technische Hochschule Aachen */
+/*      Purpose  :                                                            */
+/*                                                                            */
+/*                                                 Copyright (c)  1998-2001   */
+/*                                                 by  RWTH-Aachen, Germany   */
+/*                                                 All rights reserved.       */
+/*                                             .                              */
+/*============================================================================*/
+/*                                                                            */
+/*    THIS SOFTWARE IS PROVIDED 'AS IS'. ANY WARRANTIES ARE DISCLAIMED. IN    */
+/*    NO CASE SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DAMAGES.    */
+/*    REDISTRIBUTION AND USE OF THE NON MODIFIED TOOLKIT IS PERMITTED. OWN    */
+/*    DEVELOPMENTS BASED ON THIS TOOLKIT MUST BE CLEARLY DECLARED AS SUCH.    */
+/*                                                                            */
+/*============================================================================*/
+// $Id: $
+
+#ifndef VmsMsgReceptor_H
+#define VmsMsgReceptor_H
+
+/*============================================================================*/
+/* FORWARD DECLARATIONS														  */
+/*============================================================================*/
+class VmsMsgHandler;
+class VmsMsgReceiver;
+
+/*============================================================================*/
+/* INCLUDES																	  */
+/*============================================================================*/
+#include "VmsConfig.h"
+#include <VistaBase/VistaBaseTypes.h>
+#include <string>
+#include <vector>
+#include <zmq.hpp>
+/*============================================================================*/
+/* CLASS DEFINITION															  */
+/*============================================================================*/
+
+class VmsMsgReceptor
+{
+public:
+	VmsMsgReceptor(zmq::context_t *pContext, const std::string strIp, const unsigned int strPort);
+
+
+	~VmsMsgReceptor(void);
+	
+	/**
+	 * Receive an incoming Message from frontend and uses the correct Message Handler 
+	 * for processing
+	 */
+	int ProcessIncomingMsg();
+	
+	/**
+	 * Register a new Handler. The ownership of the handler is passed to the receptor. All handlers will
+	 * be destructed on cleanup. ID starts by 0 
+	 */
+	int RegisterHandler(VmsMsgHandler *pHandler, int id);
+	
+
+private:
+	//List of all registered handlers
+	std::vector<VmsMsgHandler*> m_oHandlers;
+	//Incomming connection from frontend
+	VmsMsgReceiver* m_pMsgReceiver;
+};
+
+
+#endif // Include guard.
+/*============================================================================*/
+/* END OF FILE																  */
+/*============================================================================*/
